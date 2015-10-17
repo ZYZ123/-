71552d6d4aa24dde46f3339377ffa0573cdb5921
ZYZ <643093165@qq.com>
2015-10-17 18:37:32 +0800

    修改2

--- my/my/AppDelegate.m
+++ my/my/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     修改
+    修改2
     return YES;
 }
 
